4aa7b73ca0beb33973f6f9e39ca5a785beebb389
Stephane <stephane.conreaux@free.fr>
2019-01-15 16:33:03 +0100

    add biome

--- sdl_background.h
+++ sdl_background.h
@@ -7,27 +7,60 @@
 #include <SDL2/SDL_image.h>
 
 /********************************************************************/
+
+class Biome {
+public:
+    Biome(const std::string& type);
+    ~Biome();
+
+    const std::vector<int>& heights() const {
+        return heights_;
+    }
+    const std::vector<int>& surface_columns() const {
+        return surface_columns_;
+    }
+    const std::vector<int>& surface_rows() const {
+        return surface_rows_;
+    }
+
+    bool is_valid() const;
+
+    const std::string& type() const {
+        return type_;
+    }
+
+    SDL_Surface* surface(int index) {
+        return surfaces_[index];
+    }
+
+    int getType(float value) const;
+
+private:
+    std::string type_;
+    std::vector<SDL_Surface*> surfaces_;
+    std::vector<int> heights_;
+    // nb columns/row for each surface
+    // if surface is composed of 6 images (3 columns, 2 rows for instance)
+    std::vector<int> surface_columns_;
+    std::vector<int> surface_rows_;
+};
+
+/********************************************************************/
+
 /*!
  * This class is used to generate background
  */
 class BackGroundGenerator {
 public:
-    BackGroundGenerator(int width, int height);
+    BackGroundGenerator(int width, int height, Biome* biome=nullptr);
     ~BackGroundGenerator();
 
     virtual void execute(const std::string& filename, float** noise_map = nullptr) const;
 
-protected:
-    int getType(float value) const;
-
 private:
     int width_;
     int height_;
-    SDL_Window* window_;
-    SDL_Renderer* renderer_;
-
-    std::vector<SDL_Surface*> surfaces_;
-    std::vector<int> heights_;
+    Biome* biome_;
 };
 
 /********************************************************************/
